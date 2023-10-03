8bced3c1ed664fa7789f0908bcd7ab1c67a7f091
GoldOfSilver <jason27004@gmail.com>
2023-10-03 09:44:51 +0200

    Mise en place du template

---
+++ public/app/views/templates/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php include_once '../app/views/templates/partials/_head.php'; ?>
+</head>
+
+<body class="bg-gray-100 text-gray-800 font-sans leading-normal tracking-normal">
+    <!-- Header -->
+    <?php include_once '../app/views/templates/partials/_header.php'; ?>
+
+    <!-- Main -->
+    <?php include_once '../app/views/templates/partials/_main.php'; ?>
+
+    <!-- Footer -->
+    <?php include_once '../app/views/templates/partials/_footer.php'; ?>
+  </body>
+</html>
